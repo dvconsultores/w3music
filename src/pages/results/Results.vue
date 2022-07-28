65d872d1d6c3a2f09350b213b0c72e5c74c4ff1c
detextre4 <detextre4@gmail.com>
2022-07-28 11:10:37 -0500

    asd

--- src/pages/results/Results.vue
+++ src/pages/results/Results.vue
@@ -1,6 +1,6 @@
 <template>
   <section id="results" class="divcol overflow margin_global">
-    <section class="container-header divcol" style="gap:2em">
+    <section class="container-header divcol" style="gap:2em" :class="{errorClass: !results}">
       <img class="pointer back" src="@/assets/icons/back-market.svg" alt="back" style="--w:231px" @click="$router.push('/buy')">
 
       <div class="divcol">
@@ -10,12 +10,14 @@
     </section>
 
     <section class="container-content acenter isolate" :class="{errorClass: !results}">
-      <img :src="require(`@/assets/miscellaneous/${results?'success':'error'}.png`)" :alt="`${results?'success':'error'} image`">
+      <div class="container-wrapper">
+        <img id="bg-image" :src="require(`@/assets/miscellaneous/${results?'success':'error'}.png`)" :alt="`${results?'success':'error'} image`">
 
-      <v-card class="card acenter gap1" style="--bg:hsl(0, 0%, 96%, .47);--p:2em;--bs:7px 8px 24px rgba(0, 0, 0, 0.25)">
-        <img :src="require(`@/assets/icons/${results?'like':'warning'}-icon.svg`)" :alt="`${results?'success':'error'} icon`" style="--w:min(100%, 5.388125em)">
-        <h3 class="p">{{results?'THANK YOU FOR YOUR PURCHASE!':'SORRY, AN ERROR OCCURRED'}}</h3>
-      </v-card>
+        <v-card class="card acenter gap1" style="--bg:hsl(0, 0%, 96%, .47);--p:2em;--bs:7px 8px 24px rgba(0, 0, 0, 0.25)">
+          <img :src="require(`@/assets/icons/${results?'like':'warning'}-icon.svg`)" :alt="`${results?'success':'error'} icon`" style="--w:min(100%, 5.388125em)">
+          <h3 class="p">{{results?'THANK YOU FOR YOUR PURCHASE!':'SORRY, AN ERROR OCCURRED'}}</h3>
+        </v-card>
+      </div>
     </section>
   </section>
 </template>
