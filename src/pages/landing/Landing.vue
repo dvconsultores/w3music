909327b72fccb6c4f87d61a46278c6644a64cfdd
detextre4 <detextre4@gmail.com>
2022-07-29 13:38:56 -0500

    cambios

--- src/pages/landing/Landing.vue
+++ src/pages/landing/Landing.vue
@@ -4,9 +4,9 @@
     <img class="eliminarmobile" src="@/assets/images/hero.png" alt="hero" style="--w:85.5em;--h:47.125em">
     <img class="vermobile" src="@/assets/images/hero-mobile.png" alt="hero" style="--w:max(100%,24.375em);--h:37.4375em">
 
-    <img id="artist" class="floating" src="@/assets/miscellaneous/artist.svg" alt="artist" @click="$router.push('/home')">
-    <img id="other" class="floating" src="@/assets/miscellaneous/other.svg" alt="other" @click="$router.push('/home')">
-    <img id="fan" class="floating" src="@/assets/miscellaneous/fan.svg" alt="fan" @click="$router.push('/home')">
+    <img id="artist" class="floating" src="@/assets/miscellaneous/songwriters.svg" alt="artist" @click="$router.push('/home')">
+    <img id="other" class="floating" src="@/assets/miscellaneous/producers.svg" alt="other" @click="$router.push('/home')">
+    <img id="fan" class="floating" src="@/assets/miscellaneous/other.svg" alt="fan" @click="$router.push('/home')">
   </section>
 </template>
 
