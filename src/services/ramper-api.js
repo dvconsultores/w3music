84673b6168318b358c0a8c592814776c524d1048
Juan Ochando <juanochando00@gmail.com>
2023-11-03 15:31:03 -0400

    up

--- src/services/ramper-api.js
+++ src/services/ramper-api.js
@@ -1,37 +1,31 @@
-import { AUTH_PROVIDER, CHAIN,getUser, init, openWallet, sendTransaction, signIn, signOut, THEME, WALLET_PROVIDER } from "@ramper/near";
-import {functionCall} from 'near-api-js/lib/transaction'
-import Vue from 'vue'
+import { AUTH_PROVIDER, CHAIN, getUser, init, openWallet, sendTransaction, signIn, signOut, THEME, WALLET_PROVIDER } from "@ramper/near";
+import { functionCall } from "near-api-js/lib/transaction";
+import Vue from "vue";
 
 export default function RamperApi() {
   init({
-    appName: 'W3 Music',
+    appName: "W3 Music",
     chainName: CHAIN.NEAR,
     walletProviders: [],
     theme: THEME.DARK,
-    network: process.env.NETWORK,
+    network: process.env.VUE_APP_NETWORK,
     logoURI: "https://bafkreihdn254nyl5cnj7unjul4mjnp6lqrrme5a4gd6zm5ppnw7iyhxh3y.ipfs.nftstorage.link/",
-    authProviders: [
-      AUTH_PROVIDER.GOOGLE,
-      AUTH_PROVIDER.FACEBOOK,
-      AUTH_PROVIDER.EMAIL,
-      AUTH_PROVIDER.TWITTER,
-      AUTH_PROVIDER.APPLE,
-    ]
-  })
+    authProviders: [AUTH_PROVIDER.GOOGLE, AUTH_PROVIDER.FACEBOOK, AUTH_PROVIDER.EMAIL, AUTH_PROVIDER.TWITTER, AUTH_PROVIDER.APPLE],
+  });
 
-  const user = getUser()
+  const user = getUser();
 
   const item = {
     getUser,
-    getAccountId: () => user ? user.wallets.near.publicKey : null,
+    getAccountId: () => (user ? user.wallets.near.publicKey : null),
     openWallet,
     signIn,
     signOut,
     functionCall,
-    sendTransaction
-  }
+    sendTransaction,
+  };
 
-  Vue.prototype.$ramper = item
+  Vue.prototype.$ramper = item;
 }
 
 /*   when need buy
